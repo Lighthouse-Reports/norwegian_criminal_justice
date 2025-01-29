25638bd0d0d520f720286bb2e774c086012a3893
cbjoerke <93517188+cbjoerke@users.noreply.github.com>
2025-01-29 15:04:13 +0100

    Add files via upload

---
+++ NRK/mitigating_and_aggravating/mitigating_aggravating_ai_lists.py
@@ -0,0 +1,212 @@
+aggravating_list = """
+ikke innrømmet noen forseelse
+mangel på respekt
+sårbart offer
+innbrudd på privatbolig
+brudd på besøksforbud
+hatforbrytelse
+uprovosert
+drapstrusler
+trusler
+forårsaket betydelig smerte 
+risiko for liv eller helse
+hvitvasking av penger
+uaktsom oppførsel
+tidligere straffet for lignende
+ny forbrytelse kort tid etter løslatelse
+plan for fremtidige forbrytelser
+personlig vinning
+særlig farlig
+brudd på prøveløslatelse
+organisert kriminell organisasjon
+lettere straff ubetinget fengsel
+lettere straff annen paragraf
+lettere straff bot
+lettere straff betinget fengsel
+varighet
+forsvarsløst offer
+begått av flere personer
+barn involvert
+barn til stede
+voldtekt
+misbruk
+begått på offentlig sted
+generelle forebyggende hensyn
+truede arter berørt
+andre lovbrudd
+systematisk utnyttelse
+påvirket av narkotika
+gjentatte lovbrudd
+kumulative lovbrudd
+kjøring under ruspåvirkning
+kjøring uten førerkort
+uaktsom kjøring
+alvorlig fare for liv og helse
+salg av ulovlige produkter
+betydelig mengde narkotika
+trygdesvindel
+helsebedrageri
+flere straffbare forhold
+vold i nære relasjoner
+brudd på straffeloven
+trusler mot offentlig tjenestemann
+vold mot offentlig tjenestemann
+trakassering av offentlig tjenestemann
+vold i et forhold
+hærverk
+forårsaket en annens død
+bruk av narkotika
+besittelse av farlige gjenstander
+besittelse av ulovlige gjenstander
+planlagt ran
+vold på offentlig sted
+kriminalitet under prøveløslatelse
+gjengkriminalitet
+bevæpnet for konfrontasjon
+brudd på besøksforbud
+forsøk på bedrageri
+psykisk vold
+profesjonelt kriminelt nettverk
+"""
+
+mitigating_list = """
+ung alder
+offeret har delvis skyld
+av liten betydning gitt omstendighetene
+selvforsvar
+anger
+lang saksbehandlingstid
+lang liggetid
+saken har blitt gammel
+tilståelse
+ingen tidligere domfellelser
+personlige utfordringer
+personlige helseproblemer
+sårbar gjerningsperson
+mindreårig gjerningsperson
+begrenset mental kapasitet
+begrenset skade
+rus
+grov uaktsomhet
+uvitende uaktsomhet
+god mulighet for rehabilitering
+under minstestraff
+berettiget sinne
+tvang
+overhengende fare
+handlet på grunn av avhengighet
+flere formildende omstendigheter
+straffen sonet i varetekt
+straffen vil være en spesielt høy byrde
+omsorg for barn
+"""
+
+'''
+
+aggravating_list = """
+not admitted any wrongdoing
+lack of respect
+vulnerable victim
+violation of private home
+violated restraining order
+hate crime
+unprovoked
+death threats
+threats
+significantly more serious result
+significant pain
+risk to life or health
+money laundering
+reckless behavior
+previous convictions
+previous criminal record
+new crime shortly after release
+presence of children
+plan for future crimes
+personal gain
+particularly dangerous
+parole violation
+organized criminal organization
+lighter sentence unconditional prison
+lighter sentence other section
+lighter sentence fine
+lighter sentence conditional prison
+harassment of public official
+duration
+defenseless victim
+committed by several people
+children involved
+rape
+abuse
+committed in public space
+general preventive considerations
+endangered species affected
+other offenses
+systematic exploitation
+cumulative offenses
+affected by drugs
+repeat offenses
+driving under influence
+driving without license
+reckless driving
+intoxication
+serious danger to life and health
+selling illegal products
+significant amount of drugs
+welfare fraud
+medical fraud
+multiple criminal offenses
+violence in close relationship
+violation of penal code
+threats against public official
+violence against public official
+violence in a relationship
+vandalism
+causing another's death
+use of narcotics
+possession of dangerous items
+possession of illegal items
+well planned robbery
+violence in public space
+in front of children
+crime during parole
+gang crime
+armed for confrontation
+violation of restraining order
+attempted fraud
+psychological violence
+professional criminal network
+fine conditions
+"""
+
+
+mitigating_list = """
+young age
+victim partially to blame
+of little significance given circumstances
+self defense
+regret
+processing time
+no previous convictions
+personal challenges 
+personal health challenges 
+perpetrator vulnerability
+perpetrator underage
+perpetrator underage
+limited mental capacity
+limited damage
+intoxication
+ignorant negligence
+good chance for rehabilitation
+confession
+conditions for below min sentence
+attempt
+justified anger, duress, imminent danger
+acted due to dependency
+multiple mitigating circumstances
+sentence served in pre-trial detention
+punishment will be a particularly high burden
+child care
+"""
+
+'''
